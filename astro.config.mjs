37812c247904806ff89afcba26478419393fd194
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-04-30 22:25:57 +0700

    update: add github pages, add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://alfathmuqoddas.github.io",
+  base: "/viraindo-webscrapper",
   output: "static",
   integrations: [],
   vite: {
