48255844ffc8e7379580ede1f07aa06d1ee09c99
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-07-20 20:40:52 +0700

    update: add cloudflare integration, remove the existing github action to use the default one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
-  site: "https://alfathmuqoddas.github.io",
-  base: "/viraindo-webscrapper",
-  output: "static",
+  // site: "https://alfathmuqoddas.github.io",
+  // base: "/viraindo-webscrapper",
+  adapter: cloudflare(),
+  output: "server",
   integrations: [preact()],
 });
