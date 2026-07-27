0ae012f9ac249e4e17887b3af7e015774be37b8f
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-07-27 22:39:38 +0700

    update: add profile menu

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,10 +1,48 @@
+import { useState, useEffect, useRef } from "preact/hooks";
 import { useAuth } from "@/hooks/useAuth";
 import { auth, googleAuthProvider } from "@/firebase/client";
 import { signInWithPopup, signOut } from "firebase/auth";
-import { LogOut } from "lucide-preact";
+import { LogOut, ChevronDown } from "lucide-preact";
 
 export const LoginButton = () => {
   const { user, loading } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const profileDropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        profileDropdownRef.current &&
+        !profileDropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const profileMenu = [
+    {
+      label: "My Builds",
+      href: "/my-builds",
+      icon: null,
+    },
+    {
+      label: "My Profile",
+      href: "/my-profile",
+      icon: null,
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: LogOut,
+    },
+  ];
 
   const handleLogin = async () => {
     try {
@@ -31,16 +69,57 @@ export const LoginButton = () => {
 
   if (user) {
     return (
-      <div class="flex items-center gap-2">
-        <span class="text-white hidden text-sm font-medium md:block">
-          {user.name}
-        </span>
+      <div ref={profileDropdownRef} class="relative inline-block">
         <button
-          onClick={handleLogout}
-          class="text-red-400 hover:text-red-500 active:text-red-500 cursor-pointer"
+          id="loginButton"
+          class="group flex items-center gap-1 p-1 rounded-full hover:bg-gray-800 active:bg-gray-800 text-white"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
         >
-          <LogOut class="" size={16} strokeWidth={3} />
+          <figure class="size-8 rounded-full overflow-hidden shrink-0">
+            <img
+              src={user.photoURL ?? ""}
+              alt={`${user.name ?? "User"}'s profile picture`}
+              class="w-full h-full object-cover"
+              referrerPolicy="no-referrer"
+            />
+          </figure>
+          <ChevronDown
+            size={12}
+            className={`${isOpen ? "rotate-180" : ""} transition-transform duration-200`}
+          />
         </button>
+        {isOpen && (
+          <div
+            id="profileMenu"
+            class="animate-fade-in fixed inset-x-2 top-14 h-auto bg-white md:absolute md:inset-auto md:top-full md:right-0 md:mt-2 md:w-32 z-50 rounded-md shadow"
+          >
+            <ul className="p-2 text-sm" aria-labelledby="dropdownDefaultButton">
+              {profileMenu.map((menu) => (
+                <li key={menu.label}>
+                  {menu.label === "Logout" ? (
+                    <button
+                      id="logoutButton"
+                      onClick={handleLogout}
+                      className="inline-flex justify-between cursor-pointer items-center w-full p-2 hover:bg-blue-400 hover:text-white rounded-md"
+                    >
+                      {menu.label}
+                      <LogOut size={12} className="text-red-500" />
+                    </button>
+                  ) : (
+                    <a
+                      id={menu.href.replace("/", "")}
+                      href={menu.href}
+                      className="inline-flex items-center w-full p-2 hover:bg-blue-400 hover:text-white rounded-md"
+                    >
+                      {menu.label}
+                    </a>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
