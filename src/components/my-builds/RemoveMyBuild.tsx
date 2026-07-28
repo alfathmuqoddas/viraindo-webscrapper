03e230f2b4fa2342bf131b3949d2e94c38d0e51a
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-07-28 21:35:03 +0700

    update: add remove my build button

---
+++ src/components/my-builds/RemoveMyBuild.tsx
@@ -0,0 +1,61 @@
+import { useState } from "preact/hooks";
+import { useAuth } from "@/hooks/useAuth";
+import { Trash2 as Trash } from "lucide-preact";
+import { dbLite } from "@/firebase/client";
+import { doc, deleteDoc } from "firebase/firestore/lite";
+
+export const RemoveMyBuild = ({
+  id,
+  userId,
+  onSuccess,
+}: {
+  id: string | undefined;
+  userId: string | undefined;
+  onSuccess?: () => void;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useAuth();
+  const { uid } = user ?? {};
+
+  const handleClick = async (e: MouseEvent) => {
+    e.preventDefault();
+    if (!confirm("Are you sure you want to remove this build?")) return;
+    setIsLoading(true);
+    if (!id || !uid) {
+      alert("Please login to remove your build");
+      return;
+    }
+    if (userId !== uid) {
+      alert("You are not the owner of this build");
+      return;
+    }
+
+    try {
+      await deleteDoc(doc(dbLite, "pcpart_builds", id));
+      alert("Build removed successfully");
+      onSuccess?.();
+    } catch (error) {
+      console.error(error);
+      alert("Error removing the build");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <button
+      class="cursor-pointer text-red-500 hover:bg-gray-100 active:bg-gray-100 text-sm p-1.5 rounded-md bg-transparent disabled:cursor-not-allowed disabled:opacity-50 transition-colors"
+      onClick={handleClick}
+      title="Remove Build"
+      aria-label="Remove Build"
+    >
+      {isLoading ? (
+        <div className="flex items-center justify-center size-4">
+          <div className="animate-spin rounded-full size-4 border-2 border-red-500 border-t-transparent" />
+        </div>
+      ) : (
+        <Trash size={16} />
+      )}
+    </button>
+  );
+};
