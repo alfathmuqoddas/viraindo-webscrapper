3b5a84d501d220712b893e2f7a3f47da4166806f
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-08-11 08:05:12 +0700

    update: add order by in completebuild page

--- src/components/completedBuilds/CompletedBuildPage.tsx
+++ src/components/completedBuilds/CompletedBuildPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useCallback, useEffect } from "preact/hooks";
+import { ArrowUpDown } from "lucide-preact";
 import { type TBuild } from "@/type";
 import {
   collection,
@@ -22,6 +23,8 @@ type TCompletedBuildPageProps = {
   initialCursorValue: number | string | null;
 };
 
+type TOrder = "latest" | "oldest" | "highestPrice" | "lowestPrice";
+
 const PAGE_LIMIT = 12;
 
 export const CompletedBuildPage = ({
@@ -33,6 +36,7 @@ export const CompletedBuildPage = ({
     useState<(TBuild & { id: string })[]>(initialBuilds);
   const [isLoading, setIsLoading] = useState(false);
   const [hasMore, setHasMore] = useState(initialHasMore);
+  const [order, setOrder] = useState<TOrder>("latest");
   const [cursor, setCursor] = useState<
     number | string | QueryDocumentSnapshot<DocumentData> | null
   >(initialCursorValue);
@@ -43,6 +47,8 @@ export const CompletedBuildPage = ({
 
     try {
       const postRef = collection(dbLite, "pcpart_builds");
+
+      const constraints: QueryConstraint[] = [where("isPublished", "==", true)];
 
       let cursorParam:
         | QueryDocumentSnapshot<DocumentData>
@@ -56,12 +62,28 @@ export const CompletedBuildPage = ({
         cursorParam = cursor;
       }
 
-      const constraints: QueryConstraint[] = [
-        where("isPublished", "==", true),
-        orderBy("createdAt", "desc"),
-        ...(cursorParam !== null ? [startAfter(cursorParam)] : []),
-        limit(PAGE_LIMIT),
-      ];
+      switch (order) {
+        case "latest":
+          constraints.push(orderBy("createdAt", "desc"));
+          break;
+        case "oldest":
+          constraints.push(orderBy("createdAt", "asc"));
+          break;
+        case "highestPrice":
+          constraints.push(orderBy("price", "desc"));
+          break;
+        case "lowestPrice":
+          constraints.push(orderBy("price", "asc"));
+          break;
+        default:
+          constraints.push(orderBy("createdAt", "desc"));
+      }
+
+      if (cursor) {
+        constraints.push(startAfter(cursor));
+      }
+
+      constraints.push(limit(PAGE_LIMIT));
 
       const q = query(postRef, ...constraints);
       const snapshot = await getDocs(q);
@@ -87,8 +109,33 @@ export const CompletedBuildPage = ({
     }
   }, [cursor, hasMore, isLoading]);
 
+  useEffect(() => {
+    fetchBuilds();
+  }, [order]);
+
   return (
     <>
+      <div class="mb-4 flex gap-1 items-center">
+        <label for="order" class="">
+          Order By: &nbsp;
+        </label>
+        <select
+          id="order"
+          name="order"
+          class="border border-gray-200 rounded-lg px-2 py-1 bg-white"
+          onChange={(e) => {
+            setOrder(e.currentTarget.value as TOrder);
+            setCursor(null);
+            setBuilds([]);
+            setHasMore(true);
+          }}
+        >
+          <option value="latest">Latest</option>
+          <option value="oldest">Oldest</option>
+          {/* <option value="highestPrice">Highest Price</option>
+          <option value="lowestPrice">Lowest Price</option> */}
+        </select>
+      </div>
       <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
         {builds?.map((build) => (
           <BuildCard key={build.id} build={build} />
