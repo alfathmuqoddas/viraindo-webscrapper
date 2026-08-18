d3b28dc1b9aa7e49259937ac62d5fbfda37c00e6
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2026-08-18 09:48:50 +0700

    update: add pagination, and update removed useEffect from filtering logic

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -1,50 +1,56 @@
-import { useState, useEffect } from "preact/hooks";
+import { useState, useMemo } from "preact/hooks";
 import { currencyFormatter } from "@/lib/helper";
 import AddToBuilderButton from "@/components/builder/AddToBuilderButton";
 import { typeToOmit } from "@/lib/constant.mjs";
 import type { PartWithoutId } from "@/type";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-preact";
 
 export const ProductTable = ({ products }: { products?: PartWithoutId[] }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(50);
   const [query, setQuery] = useState("");
-  const [debouncedQuery, setDebouncedQuery] = useState(query);
-  const [filteredProducts, setFilteredProducts] = useState(products);
 
-  useEffect(() => {
-    const list = products ?? [];
-    setFilteredProducts(
-      list.filter((p) =>
-        p?.model?.toLowerCase().includes((debouncedQuery || "").toLowerCase()),
-      ),
+  const filteredProducts = useMemo(() => {
+    if (!products) return [];
+
+    setCurrentPage(1);
+
+    if (!query.trim()) return products;
+
+    return products?.filter(
+      (p) =>
+        p.model?.toLowerCase().includes(query.toLowerCase()) ||
+        p.brand?.toLowerCase().includes(query.toLowerCase()),
     );
-  }, [debouncedQuery, products]);
+  }, [products, query]);
 
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedQuery(query);
-    }, 500);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [query]);
+  const totalPages = Math.ceil(filteredProducts.length / pageSize);
+  const startIndex = (currentPage - 1) * pageSize;
+  const visibleProducts = filteredProducts.slice(
+    startIndex,
+    startIndex + pageSize,
+  );
 
   return (
-    <>
-      <div className="my-4">
-        <div className="flex items-center bg-white rounded-md  border-gray-300 justify-between w-full md:w-64 border">
-          <input
-            type="text"
-            value={query}
-            onInput={(e) => {
-              const target = e.target as HTMLInputElement;
-              setQuery(target.value);
-            }}
-            className="w-full px-2 py-1"
-            placeholder="Search..."
-          />
-          <span className="px-2">🔍</span>
-        </div>
+    <div className="space-y-4">
+      <div className="flex items-center bg-white rounded-md  border-gray-300 justify-between w-full md:w-64 border">
+        <input
+          type="text"
+          value={query}
+          onInput={(e) => {
+            const target = e.target as HTMLInputElement;
+            setQuery(target.value);
+          }}
+          className="w-full px-2 py-1"
+          placeholder="Search..."
+        />
+        <span className="px-2">🔍</span>
       </div>
+      {query.trim() && (
+        <p className="">
+          {filteredProducts.length} of {products?.length} product(s) found
+        </p>
+      )}
       <div className="overflow-x-auto border border-gray-300 rounded-xl w-full">
         <table className="w-full min-w-xl bg-white">
           <thead>
@@ -56,17 +62,19 @@ export const ProductTable = ({ products }: { products?: PartWithoutId[] }) => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-300">
-            {!filteredProducts || filteredProducts.length === 0 ? (
+            {!visibleProducts || visibleProducts.length === 0 ? (
               <tr className="">
                 <td colSpan={4} className="p-3 text-center text-gray-500">
                   No products found
                 </td>
               </tr>
             ) : (
-              filteredProducts.map((p, index) => {
+              visibleProducts.map((p, index) => {
                 return (
                   <tr key={index} className="">
-                    <td className="p-3 align-middle truncate">{index + 1}</td>
+                    <td className="p-3 align-middle truncate">
+                      {index + startIndex + 1}
+                    </td>
                     <td className="p-3 align-middle wrap-break-word">
                       <a
                         href={`https://google.com/search?q=${p.model}`}
@@ -96,6 +104,38 @@ export const ProductTable = ({ products }: { products?: PartWithoutId[] }) => {
           </tbody>
         </table>
       </div>
-    </>
+
+      {totalPages > 1 && (
+        <nav className="flex justify-between gap-2 items-center">
+          <button
+            className="btn btn-ghost btn-sm"
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+          >
+            <ChevronLeftIcon size={20} />
+          </button>
+          <div className="flex items-center gap-1">
+            <input
+              type="number"
+              className="px-2 py-1 rounded border border-gray-200 bg-white max-w-16"
+              value={currentPage}
+              onInput={(e) => {
+                const target = e.target as HTMLInputElement;
+                setCurrentPage(parseInt(target.value));
+              }}
+            />
+            <span>/</span>
+            <span>{totalPages}</span>
+          </div>
+          <button
+            className="btn btn-ghost btn-sm"
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+          >
+            <ChevronRightIcon size={20} />
+          </button>
+        </nav>
+      )}
+    </div>
   );
 };
